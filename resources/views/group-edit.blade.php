d8068a7647768507030d2c668342d82afb101ef3
ivan006 <ivan.copeland2015@gmail.com>
2019-08-21 13:45:46 +0200

    1

--- resources/views/group-edit.blade.php
+++ resources/views/group-edit.blade.php
@@ -68,7 +68,6 @@
             <?php echo PostAndGroupMenu(); ?>
 
             <ul>
-              <?php //dd($PostList) ?>
               <?php foreach($PostShowImSubPosts as $key => $value){?>
                 <li class="f-leaf">
                   <a href="{{$value['url']}}">
@@ -89,11 +88,6 @@
     <h2>Data</h2>
 
 
-    <!-- $ShowAttributeTypes["0"] =   'SmartDataName';
-    $ShowAttributeTypes["2"] =   'SmartDataContent'; -->
-    <?php
-    // dd( $allURLs['sub_post_store']) ;
-    ?>
     <form  id="form" enctype="multipart/form-data" name="" class="" action="{{ $allURLs['sub_post_store'] }}" method="post">
       {{csrf_field()}}
       <input class="g-bor-gre"  style="display: none;" type="text" name="form" value="data">
@@ -102,8 +96,7 @@
 
 
         <?php
-        // dd($DataShowAll);
-        // dd($DataShowAll);
+
         if (!empty($DataShowAll)) {
           function list1($Identifier,$DataShowAll,$Attr){
 
@@ -113,22 +106,17 @@
               <?php
               $IdentifierSuffix = -1;
               foreach($DataShowAll[$Attr[2]] as $key => $value2){
-                // dd($value2);
+
                 $IdentifierSuffix = $IdentifierSuffix+1;
                 $CurrentIdentifier = $Identifier."[".$Attr[2]."]"."[".$IdentifierSuffix."]";
 
-                // $SmartDataLocation = $SmartDataLocationParent."[".$Attr[2]."]".'['.g_base64_encode($key).']';
 
                 if (is_array($value2)) {
-                  // if (!isset($value2["SmartDataType"])) {
-                  // }
-                  // dd($value2);
+
                   if ($value2[$Attr[1]] == 'folder') {
                     ?>
                     <li>
-                      <?php
-                      // dd($CurrentIdentifier);
-                      ?>
+
                       <div class="g-bor-lig-gre g-pad-2px g-mar-4px">
 
                         <input class="g-bor-gre"  style="" type="text" name="<?php echo $CurrentIdentifier ?>[<?php echo $Attr[0] ?>]" value="<?php echo $value2[$Attr[0]] ?>">
@@ -155,21 +143,17 @@
                         <?php echo DataFileMenu($CurrentIdentifier,$Attr); ?>
                         <?php
                         $fileExtension = FileExtention($value2[$Attr[0]]);
-                        // dd($extention);
 
-                        // if ($value2[$Attr[1]] == 'image') {
                         if (
-                          $fileExtension=="png"
-                          or $fileExtension=="jpg"
-                          or $fileExtension=="jpeg"
-                          or $fileExtension=="png"
-                          or $fileExtension=="gif"
+                        $fileExtension=="png"
+                        or $fileExtension=="jpg"
+                        or $fileExtension=="jpeg"
+                        or $fileExtension=="png"
+                        or $fileExtension=="gif"
                         ) {
-                        ?>
+                          ?>
                           <div class="">
-                            <?php
-                            // dd($value2[$Attr[2]])
-                            ?>
+
                             <img style="max-width: 50%;" alt="Embedded Image" src="<?php echo $value2[$Attr[2]]; ?>" />
                             <textarea class="g-bor-gre "  style="display:none;" name="<?php echo $CurrentIdentifier ?>[<?php echo $Attr[2] ?>]" rows="8" ><?php echo $value2[$Attr[2]]; ?></textarea>
                           </div>
@@ -192,11 +176,11 @@
           ?>
           <div class="f-treeview" >
 
-                <?php
-                // dd($DataShowAll);
-                $Identifier = "Data";
-                list1($Identifier,$DataShowAll,$Attr);
-                ?>
+            <?php
+
+            $Identifier = "Data";
+            list1($Identifier,$DataShowAll,$Attr);
+            ?>
 
 
           </div>
