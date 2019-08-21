308c2447bd456652d8e5b98cd9b600a854c1d89b
ivan006 <ivan.copeland2015@gmail.com>
2019-08-21 12:20:11 +0200

    1

--- app/Data.php
+++ app/Data.php
@@ -3,233 +3,220 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
 use Illuminate\Support\Facades\DB;
-
 
 class Data extends Model
 {
-
-  protected $primaryKey = 'id';
-  protected $fillable = [
+    protected $primaryKey = 'id';
+    protected $fillable = [
     'name',
     'parent_id',
     'parent_type',
     'type',
     'content',
   ];
-  public function parent() {
-    return $this->morphTo();
-  }
-  public function children() {
-    return $this->morphMany('App\Data', 'parent');
-  }
 
-
-  public static function ShowID($routeParameters,$DataSig) {
-    $GroupShowID = Group::ShowID($routeParameters);
-    $PostShowID = Post::ShowID($GroupShowID,$routeParameters);
-
-    $DataShowSigPref = Data::ShowSignaturePrefix();
-    if (!empty($PostShowID)) {
-
-
-      $Post = Post::find($PostShowID);
-      if (!empty($Post)) {
-        $Data = $Post->DataChildren->where('name', $DataShowSigPref)->first();
-        if (!empty($Data)) {
-          $ShowParentID = $Data->id;
-
-        }
-      }
-
-    } elseif (!empty($GroupShowID)) {
-
-      $Group = Group::find($GroupShowID);
-      if (!empty($Group)) {
-        $Data = $Group->DataChildren->where('name', $DataShowSigPref)->first();
-        if (!empty($Data)) {
-          $ShowParentID = $Data->id;
-
-        }
-      }
-
+    public function parent()
+    {
+        return $this->morphTo();
     }
 
-    $DataSigFragments = explode("/", $DataSig);
+    public function children()
+    {
+        return $this->morphMany('App\Data', 'parent');
+    }
 
-    // ----------------
-    // querie strat
-    // ----------------
+    public static function ShowID($routeParameters, $DataSig)
+    {
+        $GroupShowID = Group::ShowID($routeParameters);
+        $PostShowID = Post::ShowID($GroupShowID, $routeParameters);
 
-    if (!empty($ShowParentID)) {
-
-      $ShowID = $ShowParentID;
-      foreach ($DataSigFragments as $key => $value) {
-
-        $DataParent = Data::find($ShowID);
-        if (!empty($DataParent)) {
-          $Data = $DataParent->children->where('name', $value)->first();
-          if (!empty($Data)) {
-
-            $ShowID = $Data->id;
-          } else {
-            $ShowID = null;
-          }
-        } else {
-
-          $ShowID = null;
+        $DataShowSigPref = Data::ShowSignaturePrefix();
+        if (!empty($PostShowID)) {
+            $Post = Post::find($PostShowID);
+            if (!empty($Post)) {
+                $Data = $Post->DataChildren->where('name', $DataShowSigPref)->first();
+                if (!empty($Data)) {
+                    $ShowParentID = $Data->id;
+                }
+            }
+        } elseif (!empty($GroupShowID)) {
+            $Group = Group::find($GroupShowID);
+            if (!empty($Group)) {
+                $Data = $Group->DataChildren->where('name', $DataShowSigPref)->first();
+                if (!empty($Data)) {
+                    $ShowParentID = $Data->id;
+                }
+            }
         }
 
-        // $ShowID = DB::select(
+        $DataSigFragments = explode('/', $DataSig);
+
+        // ----------------
+        // querie strat
+        // ----------------
+
+        if (!empty($ShowParentID)) {
+            $ShowID = $ShowParentID;
+            foreach ($DataSigFragments as $key => $value) {
+                $DataParent = Data::find($ShowID);
+                if (!empty($DataParent)) {
+                    $Data = $DataParent->children->where('name', $value)->first();
+                    if (!empty($Data)) {
+                        $ShowID = $Data->id;
+                    } else {
+                        $ShowID = null;
+                    }
+                } else {
+                    $ShowID = null;
+                }
+
+                // $ShowID = DB::select(
         //   'SELECT *
         //   FROM data a, data b
         //   WHERE a.parent_id = b.id
         //   AND a.parent_type = "App\\Data"
         //   ;'
         // );
-
-      }
-    }
-    // ----------------
-    // querie strat
-    // ----------------
-    if (isset($ShowID)) {
-      return $ShowID;
-    }
-
-
-
-  }
-
-  public static function Show($DataShowID) {
-    $DataShowAll = Data::find($DataShowID);
-
-    if (!empty($DataShowAll)) {
-      $ShowDataContent = $DataShowAll->toArray();
-
-      $Attr = Data::ShowAttributeTypes();
-
-      $result[$Attr[2]] = $ShowDataContent["content"];
-      $result[$Attr[1]] = $ShowDataContent["type"];
-      $result[$Attr[0]] = $ShowDataContent["name"];
-      $result[$Attr[4]] = $ShowDataContent["id"];
-
-      return $result;
-    }
-
-
-  }
-
-  public static function ShowSignaturePrefix() {
-
-    $result = "_data";
-    return $result;
-
-  }
-  public static function ShowAll($routeParameters) {
-
-    if(!function_exists('App\ShowHelper')){
-      function ShowHelper($Data,$Identifier) {
-        $result = array();
-        $Attr = Data::ShowAttributeTypes();
-
-        $Identifier = -1;
-        foreach ($Data as $key => $value) {
-
-          $Identifier = $Identifier+1;
-
-          $SubData = Data::find($value["id"])->children->toArray();
-
-          if ($value["type"] == "folder") {
-
-            $result[$Identifier][$Attr[2]] = ShowHelper($SubData,$Identifier);
-            $result[$Identifier][$Attr[1]] = $value["type"];
-            $result[$Identifier][$Attr[0]] = $value["name"];
-            $result[$Identifier][$Attr[4]] = $value["id"];
-          } else {
-            $result[$Identifier] = Data::Show($value["id"]);
-          }
-
+            }
         }
-        return  $result;
-      }
+        // ----------------
+        // querie strat
+        // ----------------
+        if (isset($ShowID)) {
+            return $ShowID;
+        }
     }
 
-    $GroupShowID = Group::ShowID($routeParameters);
-    $PostShowID = Post::ShowID($GroupShowID,$routeParameters);
+    public static function Show($DataShowID)
+    {
+        $DataShowAll = Data::find($DataShowID);
 
-    $Identifier = null;
-    if (!empty($PostShowID)) {
-      $BaseData = Post::find($PostShowID)->DataChildren->toArray();
-    } elseif (!empty($GroupShowID)) {
-      $BaseData = Group::find($GroupShowID)->DataChildren->toArray();
+        if (!empty($DataShowAll)) {
+            $ShowDataContent = $DataShowAll->toArray();
+
+            $Attr = Data::ShowAttributeTypes();
+
+            $result[$Attr[2]] = $ShowDataContent['content'];
+            $result[$Attr[1]] = $ShowDataContent['type'];
+            $result[$Attr[0]] = $ShowDataContent['name'];
+            $result[$Attr[4]] = $ShowDataContent['id'];
+
+            return $result;
+        }
     }
-    $Show =   ShowHelper($BaseData,$Identifier);
-    return $Show;
-  }
 
-  public static function ShowAttributeTypes() {
-    $ShowAttributeTypes = array  (
-    '0'=>'name',
-    '1'=>'type',
-    '2'=>'content',
-    '3'=>'action',
-    '4'=>'id',
-    '5'=>'subtype',
-    '6'=>'add',
+    public static function ShowSignaturePrefix()
+    {
+        $result = '_data';
 
+        return $result;
+    }
+
+    public static function ShowAll($routeParameters)
+    {
+        if (!function_exists('App\ShowHelper')) {
+            function ShowHelper($Data, $Identifier)
+            {
+                $result = array();
+                $Attr = Data::ShowAttributeTypes();
+
+                $Identifier = -1;
+                foreach ($Data as $key => $value) {
+                    $Identifier = $Identifier + 1;
+
+                    $SubData = Data::find($value['id'])->children->toArray();
+
+                    if ('folder' == $value['type']) {
+                        $result[$Identifier][$Attr[2]] = ShowHelper($SubData, $Identifier);
+                        $result[$Identifier][$Attr[1]] = $value['type'];
+                        $result[$Identifier][$Attr[0]] = $value['name'];
+                        $result[$Identifier][$Attr[4]] = $value['id'];
+                    } else {
+                        $result[$Identifier] = Data::Show($value['id']);
+                    }
+                }
+
+                return  $result;
+            }
+        }
+
+        $GroupShowID = Group::ShowID($routeParameters);
+        $PostShowID = Post::ShowID($GroupShowID, $routeParameters);
+
+        $Identifier = null;
+        if (!empty($PostShowID)) {
+            $BaseData = Post::find($PostShowID)->DataChildren->toArray();
+        } elseif (!empty($GroupShowID)) {
+            $BaseData = Group::find($GroupShowID)->DataChildren->toArray();
+        }
+        $Show = ShowHelper($BaseData, $Identifier);
+
+        return $Show;
+    }
+
+    public static function ShowAttributeTypes()
+    {
+        $ShowAttributeTypes = array(
+    '0' => 'name',
+    '1' => 'type',
+    '2' => 'content',
+    '3' => 'action',
+    '4' => 'id',
+    '5' => 'subtype',
+    '6' => 'add',
     );
 
-    return $ShowAttributeTypes;
-  }
-  public static function ShowActions() {
-    $ShowActions["SelectedSmartDataItem"] =   'Selected';
-    return $ShowActions;
-  }
+        return $ShowAttributeTypes;
+    }
 
+    public static function ShowActions()
+    {
+        $ShowActions['SelectedSmartDataItem'] = 'Selected';
 
-  public static function Store($request) {
+        return $ShowActions;
+    }
 
-    function StoreHelperStore($Action,$Data,$Attr) {
-      if (isset($Data[$Attr[2]])) {
+    public static function Store($request)
+    {
+        function StoreHelperStore($Action, $Data, $Attr)
+        {
+            if (isset($Data[$Attr[2]])) {
+                foreach ($Data[$Attr[2]] as $key => $value) {
+                    if ('folder' == $value[$Attr[1]]) {
+                        if (isset($value[$Attr[3]])) {
+                            $Action = $value[$Attr[3]];
+                        }
 
-        foreach($Data[$Attr[2]] as $key => $value) {
-
-          if ($value[$Attr[1]]=="folder"){
-            if (isset($value[$Attr[3]]) ) {
-              $Action = $value[$Attr[3]];
-            }
-
-            switch ($Action) {
+                        switch ($Action) {
               case 'update':
               if (!empty($value[$Attr[4]])) {
-                Data::find($value[$Attr[4]])
+                  Data::find($value[$Attr[4]])
                 ->update([
-                'name'=>$value[$Attr[0]],
+                'name' => $value[$Attr[0]],
                 ]);
               }
               break;
               case 'create_folder':
 
-              $name = $value[$Attr[6]]["folder"];
+              $name = $value[$Attr[6]]['folder'];
               $parent_id = $value[$Attr[4]];
               $parent_type = "App\Data";
-              $type = "folder";
-              $content = "null";
+              $type = 'folder';
+              $content = 'null';
 
-              Data::Add($name, $parent_id,$parent_type,$type,$content);
+              Data::Add($name, $parent_id, $parent_type, $type, $content);
               $Action = null;
               break;
               case 'create_file':
 
-              $name = $value[$Attr[6]]["file"];
+              $name = $value[$Attr[6]]['file'];
               $parent_id = $value[$Attr[4]];
               $parent_type = "App\Data";
-              $type = "file";
-              $content = "null";
+              $type = 'file';
+              $content = 'null';
 
-              Data::Add($name, $parent_id,$parent_type,$type,$content);
+              Data::Add($name, $parent_id, $parent_type, $type, $content);
 
               $Action = null;
               break;
@@ -238,20 +225,19 @@ class Data extends Model
 
               break;
             }
-            StoreHelperStore($Action, $value,$Attr);
-          } else {
+                        StoreHelperStore($Action, $value, $Attr);
+                    } else {
+                        if (isset($value[$Attr[3]])) {
+                            $Action = $value[$Attr[3]];
+                        }
 
-            if (isset($value[$Attr[3]])) {
-              $Action = $value[$Attr[3]];
-            }
-
-            switch ($Action) {
+                        switch ($Action) {
               case 'update':
               if (!empty($value[$Attr[4]])) {
-                Data::find($value[$Attr[4]])
+                  Data::find($value[$Attr[4]])
                 ->update([
-                'name'=>$value[$Attr[0]],
-                'content'=>$value[$Attr[2]],
+                'name' => $value[$Attr[0]],
+                'content' => $value[$Attr[2]],
                 ]);
               }
               break;
@@ -260,24 +246,24 @@ class Data extends Model
 
               break;
             }
-          }
+                    }
+                }
+            }
         }
-      }
+        $Attr = Data::ShowAttributeTypes();
+        $Data = $request->get('Data');
+
+        StoreHelperStore(null, $Data, $Attr);
     }
-    $Attr = Data::ShowAttributeTypes();
-    $Data = $request->get("Data");
 
-    StoreHelperStore(null,$Data,$Attr);
-  }
-  public static function Add ($name, $parent_id,$parent_type,$type,$content){
-
-    Data::create([
-    'name'=>$name,
-    'parent_id'=>$parent_id,
-    'parent_type'=>$parent_type,
-    'type'=>$type,
-    'content'=>$content,
+    public static function Add($name, $parent_id, $parent_type, $type, $content)
+    {
+        Data::create([
+    'name' => $name,
+    'parent_id' => $parent_id,
+    'parent_type' => $parent_type,
+    'type' => $type,
+    'content' => $content,
     ]);
-  }
-
+    }
 }
