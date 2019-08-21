1c25faded4e031442feb4353c6377b48a58f7b03
ivan006 <ivan.copeland2015@gmail.com>
2019-08-21 13:48:50 +0200

    1

--- resources/views/group-edit.blade.php
+++ resources/views/group-edit.blade.php
@@ -68,13 +68,17 @@
             <?php echo PostAndGroupMenu(); ?>
 
             <ul>
-              <?php foreach($PostShowImSubPosts as $key => $value){?>
+              <?php
+              foreach ($PostShowImSubPosts as $key => $value) {
+                ?>
                 <li class="f-leaf">
                   <a href="{{$value['url']}}">
                     {{$key}}
                   </a>
                 </li>
-              <?php }?>
+                <?php
+              }
+              ?>
 
             </ul>
           </li>
@@ -98,69 +102,69 @@
         <?php
 
         if (!empty($DataShowAll)) {
-          function list1($Identifier,$DataShowAll,$Attr){
-
-
+          function list1($Identifier, $DataShowAll, $Attr)
+          {
             ?>
             <ul>
               <?php
               $IdentifierSuffix = -1;
-              foreach($DataShowAll[$Attr[2]] as $key => $value2){
-
-                $IdentifierSuffix = $IdentifierSuffix+1;
-                $CurrentIdentifier = $Identifier."[".$Attr[2]."]"."[".$IdentifierSuffix."]";
-
+              foreach ($DataShowAll[$Attr[2]] as $key => $value2) {
+                $IdentifierSuffix = $IdentifierSuffix + 1;
+                $CurrentIdentifier = $Identifier.'['.$Attr[2].']'.'['.$IdentifierSuffix.']';
 
                 if (is_array($value2)) {
-
-                  if ($value2[$Attr[1]] == 'folder') {
+                  if ('folder' == $value2[$Attr[1]]) {
                     ?>
                     <li>
 
                       <div class="g-bor-lig-gre g-pad-2px g-mar-4px">
 
-                        <input class="g-bor-gre"  style="" type="text" name="<?php echo $CurrentIdentifier ?>[<?php echo $Attr[0] ?>]" value="<?php echo $value2[$Attr[0]] ?>">
-                        <input class=""  style="display:none;" type="text" name="<?php echo $CurrentIdentifier ?>[<?php echo $Attr[1] ?>]" value="<?php echo $value2[$Attr[1]] ?>">
-                        <input class=""  style="display:none;" type="text" name="<?php echo $CurrentIdentifier ?>[<?php echo $Attr[4] ?>]" value="<?php echo $value2[$Attr[4]] ?>">
-                        <?php echo DataFolderMenu($CurrentIdentifier,$Attr); ?>
+                        <input class="g-bor-gre"  style="" type="text" name="<?php echo $CurrentIdentifier; ?>[<?php echo $Attr[0]; ?>]" value="<?php echo $value2[$Attr[0]]; ?>">
+                        <input class=""  style="display:none;" type="text" name="<?php echo $CurrentIdentifier; ?>[<?php echo $Attr[1]; ?>]" value="<?php echo $value2[$Attr[1]]; ?>">
+                        <input class=""  style="display:none;" type="text" name="<?php echo $CurrentIdentifier; ?>[<?php echo $Attr[4]; ?>]" value="<?php echo $value2[$Attr[4]]; ?>">
+                        <?php echo DataFolderMenu($CurrentIdentifier, $Attr); ?>
                       </div>
 
 
 
-                      <?php
-                      list1($CurrentIdentifier,$value2,$Attr);
-                      ?>
+                      <?php list1($CurrentIdentifier, $value2, $Attr); ?>
                     </li>
 
 
-                  <?php  } else { ?>
+                    <?php
+                  } else {
+                    ?>
                     <li class="f-leaf">
                       <div class="g-bor-lig-gre g-pad-2px g-mar-4px">
 
-                        <input class="g-bor-gre"  style="" type="text" name="<?php echo $CurrentIdentifier ?>[<?php echo $Attr[0] ?>]" value="<?php echo $value2[$Attr[0]] ?>">
-                        <input class=""  style="display:none;" type="text" name="<?php echo $CurrentIdentifier ?>[<?php echo $Attr[1] ?>]" value="<?php echo $value2[$Attr[1]] ?>">
-                        <input class=""  style="display:none;" type="text" name="<?php echo $CurrentIdentifier ?>[<?php echo $Attr[4] ?>]" value="<?php echo $value2[$Attr[4]] ?>">
-                        <?php echo DataFileMenu($CurrentIdentifier,$Attr); ?>
+                        <input class="g-bor-gre"  style="" type="text" name="<?php echo $CurrentIdentifier; ?>[<?php echo $Attr[0]; ?>]" value="<?php echo $value2[$Attr[0]]; ?>">
+                        <input class=""  style="display:none;" type="text" name="<?php echo $CurrentIdentifier; ?>[<?php echo $Attr[1]; ?>]" value="<?php echo $value2[$Attr[1]]; ?>">
+                        <input class=""  style="display:none;" type="text" name="<?php echo $CurrentIdentifier; ?>[<?php echo $Attr[4]; ?>]" value="<?php echo $value2[$Attr[4]]; ?>">
+                        <?php echo DataFileMenu($CurrentIdentifier, $Attr); ?>
                         <?php
                         $fileExtension = FileExtention($value2[$Attr[0]]);
 
                         if (
-                        $fileExtension=="png"
-                        or $fileExtension=="jpg"
-                        or $fileExtension=="jpeg"
-                        or $fileExtension=="png"
-                        or $fileExtension=="gif"
+                        'png' == $fileExtension
+                        or 'jpg' == $fileExtension
+                        or 'jpeg' == $fileExtension
+                        or 'png' == $fileExtension
+                        or 'gif' == $fileExtension
                         ) {
                           ?>
                           <div class="">
 
                             <img style="max-width: 50%;" alt="Embedded Image" src="<?php echo $value2[$Attr[2]]; ?>" />
-                            <textarea class="g-bor-gre "  style="display:none;" name="<?php echo $CurrentIdentifier ?>[<?php echo $Attr[2] ?>]" rows="8" ><?php echo $value2[$Attr[2]]; ?></textarea>
+                            <textarea class="g-bor-gre "  style="display:none;" name="<?php echo $CurrentIdentifier; ?>[<?php echo $Attr[2]; ?>]" rows="8" ><?php echo $value2[$Attr[2]]; ?></textarea>
                           </div>
-                        <?php } else { ?>
+                          <?php
+                        } else {
+                          ?>
 
-                          <textarea class="g-bor-gre f-res-ver"  style="width:100%;" name="<?php echo $CurrentIdentifier ?>[<?php echo $Attr[2] ?>]" rows="8" ><?php echo $value2[$Attr[2]]; ?></textarea>
-                        <?php } ?>
+                          <textarea class="g-bor-gre f-res-ver"  style="width:100%;" name="<?php echo $CurrentIdentifier; ?>[<?php echo $Attr[2]; ?>]" rows="8" ><?php echo $value2[$Attr[2]]; ?></textarea>
+                          <?php
+                        }
+                        ?>
                       </div>
                     </li>
 
@@ -172,20 +176,18 @@
             </ul>
             <?php
           }
-
           ?>
           <div class="f-treeview" >
 
             <?php
 
-            $Identifier = "Data";
-            list1($Identifier,$DataShowAll,$Attr);
+            $Identifier = 'Data';
+            list1($Identifier, $DataShowAll, $Attr);
             ?>
 
 
           </div>
           <?php
-
         }
         ?>
       </div>
