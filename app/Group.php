fed86da89f7431400a941e380b72df86f1462440
ivan006 <ivan.copeland2015@gmail.com>
2019-08-21 12:32:13 +0200

    1

--- app/Group.php
+++ app/Group.php
@@ -15,37 +15,24 @@ class Group extends Model
   protected $fillable = [
     'name',
   ];
-
+  
   public function PostChildren() {
     return $this->morphMany('App\Post', 'parent');
   }
   public function DataChildren() {
     return $this->morphMany('App\Data', 'parent');
   }
-  // public static function ShowBaseLocation() {
-  //   // return "storage/app/public/";
-  //   return base_path()."/storage/app/public/";
-  // }
-  // above is not needed
+
   public static function ShowAll() {
-    // $ShowBaseLocation = Group::ShowBaseLocation();
-    // above is not needed
-    // $dataNameList = scandir($ShowBaseLocation);
+
     $dataNameList = Group::all();
     $result = array();
-    // dd($dataNameList);
+
     foreach ($dataNameList as $key => $value) {
 
-      // $dataLocation = $ShowBaseLocation . "/" . $value;
-      // above is not needed
-      // if (!in_array($value,array(".","..")) && is_dir($dataLocation) )  {
-      // above is not needed
 
-            // $url = str_replace($ShowBaseLocation."/", "", $dataLocation);
-            // above is not needed
-            $result[$value->name]["url"] = route('NetworkC.show', $value->name);
-      // }
-      // above is not needed
+      $result[$value->name]["url"] = route('NetworkC.show', $value->name);
+
     }
 
     return $result;
@@ -56,20 +43,16 @@ class Group extends Model
     return $var;
   }
   public static function ShowID($routeParameters){
-    // dd($routeParameters);
+
     $ShowID = Group::where('name', $routeParameters[0])->first()->id;
 
     return $ShowID;
 
   }
   public static function Add($request) {
-    // dd($request);
+
     $GroupName = $request["name"];
-    // dd($GroupName);
-    // dd($request);
-    // Group::create([
-    //   'name'=>$GroupName,
-    // ]);
+
     $var = new Group;
     $var->name = $GroupName;
     $var->save();
@@ -83,8 +66,8 @@ class Group extends Model
     $type = "folder";
     $content = "null";
 
-    // Data::Create($name,$parent_id,$parent_type,$type,$content)
+
     Data::Add($name, $parent_id,$parent_type,$type,$content);
-    // mkdir(GroupM::ShowBaseLocation()."/".$request->get('name'));
+
   }
 }
