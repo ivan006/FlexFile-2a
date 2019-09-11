a73e502fd79f12202c6f48cbb1e5851a2d3b47c7
ivan006 <ivan.copeland2015@gmail.com>
2019-09-11 12:37:10 +0200

    1

--- public/test/A/LowestNumberFinder.php
+++ public/test/A/LowestNumberFinder.php
@@ -5,62 +5,17 @@ class LowestNumberFinder {
     public function getLowest(string $numbers): string {
 
         // todo
-        $result = null;
-        $numbers = str_replace(' ', '', $numbers);
-        $numbers = explode(',', $numbers);
-        // $numbers = array_map('intval', $numbers);
-        // sort($numbers);
-        if (1==1) {
-          // code...
-          $numbersAsString = json_encode($numbers, JSON_PRETTY_PRINT);
-          $numbersAsString = '<pre>'.$numbersAsString.'</pre>';
-          $result .= $numbersAsString.'<br>';
-        }
+        $range1 = $numbers;
+        $range1 = str_replace(' ', '', $range1);
+        $range1 = explode(',', $range1);
+        $range2Lowest = min($range1)-1;
+        $range2Highest = max($range1)+1;
+        $range2 = range($range2Lowest,$range2Highest);
+        $missing = array_diff($range2,$range1);
+        $missing = array_filter($missing, function ($x) { return $x > 0; });
+        $missingLowest = min($missing);
 
-
-        $lowestNumber = min($numbers);
-        $highestNumber = max($numbers);
-
-        $result .= 'lowerst number: '.$lowestNumber.'<br>';
-        $result .= 'highest number: '.$highestNumber.'<br>';
-
-        $fullRange = range($lowestNumber,$highestNumber);
-        if (1==1) {
-          // code...
-          $rangeAsString = json_encode($fullRange, JSON_PRETTY_PRINT);
-          $rangeAsString = '<pre>'.$rangeAsString.'</pre>';
-          $result .= $rangeAsString.'<br>';
-        }
-
-        $rangeDif = array_diff($fullRange,$numbers);
-
-        if (1==1) {
-          // code...
-          $rangeDifAsString = json_encode($rangeDif, JSON_PRETTY_PRINT);
-          $rangeDifAsString = '<pre>'.$rangeDifAsString.'</pre>';
-          $result .= $rangeDifAsString.'<br>';
-        }
-
-        $lowestMissing = array_filter($rangeDif, function ($x) { return $x > 0; });
-
-
-        if (1==1) {
-          // code...
-          $lowestMissingAsString = json_encode($lowestMissing, JSON_PRETTY_PRINT);
-          $lowestMissingAsString = '<pre>'.$lowestMissingAsString.'</pre>';
-          $result .= $lowestMissingAsString.'<br>';
-        }
-
-        $lowestMissing = min($lowestMissing);
-
-
-        if (1==1) {
-          // code...
-          $result .= 'lowest missing: '.$lowestMissing.'<br>';
-        }
-
-
-        return $result;
+        return $missingLowest;
     }
 
 
